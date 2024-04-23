0222f86d47315024dbeec1df048025d799418daa
gykim93 <gykim1993@gmail.com>
2024-04-23 14:44:16 +0900

    App class add

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,11 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요");
+        new App().run();
+    }
+}
+class App{
+    void run(){
+        System.out.println("==프로그램 실행==");
     }
 }
