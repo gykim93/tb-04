f1d2d38278ad02497a62eadc6388e71ea2f97c9b
gykim93 <gykim1993@gmail.com>
2024-04-26 13:51:30 +0900

    Rq class ì

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class App {
+public class App {
     Scanner scanner;
     int lastQuotationId;
     List<Quotation> quotations;
@@ -18,25 +18,25 @@ class App {
     void run() {
         System.out.println("== 명언 앱 ==");
 
-
-        List<Quotation> quotations = new ArrayList<>();
-
         while (true) {
-            System.out.println("명령) ");
+            System.out.print("명령) ");
 
             String cmd = scanner.nextLine();
+
+            Rq rq = new Rq(cmd);
+
+            System.out.println("rq.getAction : " + rq.getAction());
+            System.out.println("rq.getParamAsInt : " + rq.getParamAsInt("id", 0));
 
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
                 actionWrite();
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
-
             } else if (cmd.equals("목록")) {
                 actionList();
             } else if (cmd.startsWith("삭제?")) {
                 actionRemove(cmd);
-            } else if (cmd.startsWith("수정?")) {
+            }else if(cmd.startsWith("수정?")){
                 actionModify(cmd);
             }
         }
@@ -50,11 +50,12 @@ class App {
         String authorName = scanner.nextLine();
 
         lastQuotationId++;
-
         int id = lastQuotationId;
 
         Quotation quotation = new Quotation(id, content, authorName);
         quotations.add(quotation);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
     }
 
     void actionList() {
@@ -62,41 +63,37 @@ class App {
 
         System.out.println("----------------------");
 
-        if (quotations.isEmpty()) System.out.println("등록된 명언이 없습니다.");
+        if (quotations.isEmpty())
+            System.out.println("등록된 명언이 없습니다.");
 
         for (int i = quotations.size() - 1; i >= 0; i--) {
             Quotation quotation = quotations.get(i);
             System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
         }
-    } //"1"
+    }
 
     void actionRemove(String cmd) {
         int id = getParamAsint(cmd, "id", 0);
-        if (id == 0) {
+        if(id == 0){
             System.out.println("id를 정확히 입력해주세요.");
             return; //함수를 끝낸다.
         }
-
-        System.out.printf("%d번 명언을 삭제합니다\n", id);
+        System.out.printf("%d번 명언을 삭제합니다.\n", id);
     }
-
     void actionModify(String cmd) {
         int id = getParamAsint(cmd, "id", 0);
-        if (id == 0) {
+        if(id == 0){
             System.out.println("id를 정확히 입력해주세요.");
             return; //함수를 끝낸다.
         }
-
-        System.out.printf("%d번 명언을 수정합니다\n", id);
+        System.out.printf("%d번 명언을 수정합니다.\n", id);
     }
-
-    int getParamAsint(String cmd, String paramName, int defaultValue) {
+    int getParamAsint(String cmd, String paraName, int defalutValue){
         String[] cmdBits = cmd.split("\\?", 2);
         String queryString = cmdBits[1];
 
         String[] queryStringBits = queryString.split("&");
 
-        int id = 0;
         for (int i = 0; i < queryStringBits.length; i++) {
             String queryParamStr = queryStringBits[i];
 
@@ -105,16 +102,16 @@ class App {
             String _paramName = queryParamStrBits[0];
             String paramValue = queryParamStrBits[1];
 
-            if (_paramName.equals(paramName)) {
+            if (_paramName.equals(paraName)) {
                 try {
-                    // 문제가 없을 경우
+                    //문제가 없을 경우
                     return Integer.parseInt(paramValue);
                 } catch (NumberFormatException e) {
-                    // 문제가 생긴경우
-                    return defaultValue;
+                    //문제가 생긴 경우
+                    return defalutValue;
                 }
             }
         }
-        return defaultValue;
+        return defalutValue;
     }
 }
